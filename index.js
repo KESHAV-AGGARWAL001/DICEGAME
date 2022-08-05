9b0cf969675cd7ec3d633e740252f09587db4454
keshav <silentcoder45@gmail.com>
2022-08-05 12:03:23 +0530

    adding some new things and made some changes

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ console.log("the first random number is" + number1);
 console.log("the second random number is" + number2);
 
 function start() {
-  document.getElementById("image1").src = "image" + `${number1}` + ".jpg";
+  document.getElementById("image1").src = `image${number1}.jpg`;
   document.getElementById("actualsum").innerText = number1;
-  document.getElementById("image2").src = "image" + `${number2}` + ".jpg";
+  document.getElementById("image2").src = `image${number2}.jpg`;
   document.getElementById("actualsum").innerText = number1 + number2;
   if (document.getElementById("actualsum") === "12") {
     document.getElementById("finalresult").innerHTML =
